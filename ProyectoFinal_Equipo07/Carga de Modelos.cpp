c51ea5aebdb0d67d10e3af6a2426b826384a8b26
Franco Ruiz <francoruiz287@gmail.com>
2025-09-17 16:25:25 -0600

    Prueba de Rama de Franco

--- ProyectoFinal_Equipo07/Carga de Modelos.cpp
+++ ProyectoFinal_Equipo07/Carga de Modelos.cpp
@@ -54,7 +54,7 @@ int main( )
     glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
     
     // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, u8"UwU", nullptr, nullptr );
+    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, u8"UwU OwO", nullptr, nullptr );
     
     if ( nullptr == window )
     {
